c0eb2dce625c1259f321836bf873af505bb58c60
farges <farges@cbr3s2.42barcelona.com>
2024-10-13 21:38:55 +0200

    add

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: farges <farges@student.42barcelona.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 17:09:45 by farges            #+#    #+#             */
-/*   Updated: 2024/10/13 17:09:45 by farges           ###   ########.fr       */
+/*   Updated: 2024/10/13 21:38:29 by farges           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,9 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (s1[i] != (unsigned char)c)
-			i++;
-		else
-			return (&s1[i]);
+		if (s1[i] == (unsigned char)c)
+			return ((void *)&local_s[i]);
+		i++;
 	}
 	return (NULL);
 }
