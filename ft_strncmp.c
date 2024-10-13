6901435c0c8ad0a226a03aba99a9cb5e8a225b48
farges <farges@cbr3s2.42barcelona.com>
2024-10-13 21:32:00 +0200

    add

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: farges <farges@student.42barcelona.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 17:09:45 by farges            #+#    #+#             */
-/*   Updated: 2024/10/13 17:09:45 by farges           ###   ########.fr       */
+/*   Updated: 2024/10/13 21:31:46 by farges           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
+	size_t	i;
 
+	if (!s1 || !s2)
+		return (0);
+	if (n == 0)
+		return (0);
 	i = 0;
-	while (i < n && (s1[i] || s2[i]))
+	while (i < (n - 1) && *s1 == *s2 && (*s1 || *s2))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		s1++;
+		s2++;
 		i++;
 	}
-	return (0);
+	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
 }
