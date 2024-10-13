4f309774603e22717eed6d680b0b47a8b9c019f1
farges <farges@cbr3s2.42barcelona.com>
2024-10-13 21:11:49 +0200

    add

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: farges <farges@student.42barcelona.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 17:09:45 by farges            #+#    #+#             */
-/*   Updated: 2024/10/13 17:09:45 by farges           ###   ########.fr       */
+/*   Updated: 2024/10/13 21:11:35 by farges           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,11 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] != ((unsigned char) c))
-			i ++;
-		else
+		if (s[i] == ((unsigned char) c))
 			return ((char *) &s[i]);
+		i++;
 	}
+	if ((unsigned char)c == '\0')
+		return ((char *)&s[i]);
 	return (NULL);
 }
