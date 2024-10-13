fd1bef7aab046865d18c6bf5903ce2f699dc95b1
farges <farges@cbr3s2.42barcelona.com>
2024-10-13 21:18:01 +0200

    add

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: farges <farges@student.42barcelona.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 17:09:45 by farges            #+#    #+#             */
-/*   Updated: 2024/10/13 18:03:57 by farges           ###   ########.fr       */
+/*   Updated: 2024/10/13 21:17:49 by farges           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ char	*ft_strrchr(const char *s, int c)
 	int	len;
 
 	len = 0;
-	if (s == NULL)
-		return (NULL);
 	if (c == '\0')
 		return ((char *)s + len);
+	if (s == NULL)
+		return (NULL);
 	len = ft_strlen((char *)s);
 	i = len - 1;
 	while (i >= 0 && s[i])
