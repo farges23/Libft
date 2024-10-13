74c8bc9d68c251a1f6f4021ff2e825e1615b8641
farges <farges@cbr3s2.42barcelona.com>
2024-10-13 20:48:07 +0200

    add

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,30 +6,47 @@
 /*   By: farges <farges@student.42barcelona.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 17:09:45 by farges            #+#    #+#             */
-/*   Updated: 2024/10/13 20:18:44 by farges           ###   ########.fr       */
+/*   Updated: 2024/10/13 20:25:35 by farges           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aybelhaj <aybelhaj@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 17:54:51 by aybelhaj          #+#    #+#             */
+/*   Updated: 2024/10/12 18:29:47 by aybelhaj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
-	int		j;
+	size_t	j;
+	size_t	n;
 
 	if (!big)
-		return (NULL);
+		return (0);
 	if (*little == '\0')
 		return ((char *)big);
 	i = 0;
-	while ((i < len) && (big[i]))
+	n = ft_strlen(little);
+	while (i < len)
 	{
 		j = 0;
-		while (big[i + j] == little[j] && (i + j) < len)
+		while (big[i + j] == little[j] && len > (i + j))
 		{
-			if (big[i] == little[0])
-				return ((char *)&big[i]);
 			j++;
+			if (n == j)
+				return ((char *)&big[i]);
 		}
 		i++;
 	}
