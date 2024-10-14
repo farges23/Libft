8c1704a3be6df6f708161a9c2b2021e0a1826be6
farges <farges@cbr10s3.42barcelona.com>
2024-10-14 04:20:40 +0200

    add

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,13 +6,11 @@
 /*   By: farges <farges@student.42barcelona.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 17:09:45 by farges            #+#    #+#             */
-/*   Updated: 2024/10/13 21:04:58 by farges           ###   ########.fr       */
+/*   Updated: 2024/10/14 04:20:22 by farges           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-size_t	ft_strlen(const char *s);
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
