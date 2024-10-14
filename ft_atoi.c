5d8193b49ebd25fb003000d2dd5d8ba513b058a4
farges <farges@cbr10s3.42barcelona.com>
2024-10-14 03:49:21 +0200

    add

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: farges <farges@student.42barcelona.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 17:03:52 by farges            #+#    #+#             */
-/*   Updated: 2024/10/13 18:04:34 by farges           ###   ########.fr       */
+/*   Updated: 2024/10/14 03:49:07 by farges           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,10 @@ int	ft_atoi(const char *nptr)
 	result = 0;
 	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
-	if (nptr[i] == '-')
-	{
-		signe = -1;
-		i++;
-	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
+	if (nptr[i] == '-' || nptr[i] == '+')
+		if (str[i++] == '-')
+			signe = -1;
+	while (ft_isdigit(nptr[i]))
 	{
 		result = result * 10 + (nptr[i] - '0');
 		i++;
